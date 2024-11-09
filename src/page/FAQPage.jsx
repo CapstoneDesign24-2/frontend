7dd5854adf316253ce35c6eec3ff1944c2ff529f
simjaesung <tlawotjd4285@gmail.com>
2024-11-09 18:56:04 +0900

    feat: 🎸 faq 페이지 제작

--- src/page/FAQPage.jsx
+++ src/page/FAQPage.jsx
@@ -2,10 +2,39 @@ import Sidebar from '../component/sidebar';
 import '../css/FAQPage.css';
 
 const FAQPage = () => {
+  const faqItems = [
+    {
+      question: '언어 변경은 어떻게 하나요?',
+      answer: '오른쪽 상단 화살표를 누르시면 KOR / 中国人/ Việt 중에 언어를 선택하실 수 있습니다.',
+    },
+    {
+      question: '저녁 뭐 먹을까요...',
+      answer: '오른쪽 상단 화살표를 누르시면 KOR / 中国人/ Việt 중에 언어를 선택하실 수 있습니다.',
+    },
+    {
+      question: '내용 증명 로직 부분 디자인이 너무 막막해요...',
+      answer: '오른쪽 상단 화살표를 누르시면 KOR / 中国人/ Việt 중에 언어를 선택하실 수 있습니다.',
+    },
+    {
+      question: '여기도 10개 보이게 할까요?',
+      answer: '오른쪽 상단 화살표를 누르시면 KOR / 中国人/ Việt 중에 언어를 선택하실 수 있습니다.',
+    },
+    {
+      question: '심플한 걸 좋아해서 심플하게 하고 있는데 너무 심플해서 미쳐버릴 것 같아요.',
+      answer: '오른쪽 상단 화살표를 누르시면 KOR / 中国人/ Việt 중에 언어를 선택하실 수 있습니다.',
+    },
+  ];
   return (
     <div className="faq-page">
       <Sidebar />
-      <div className="content">{}</div>
+      <div className="content">
+        {faqItems.map((item, index) => (
+          <div key={index} className="faq-item">
+            <div className="question">Q. {item.question}</div>
+            <div className="answer">A. {item.answer}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
